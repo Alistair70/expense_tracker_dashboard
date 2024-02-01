63610b8eba2ef1fdead5215ec0a91ea9cae5df7a
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2024-02-01 14:07:21 -0500

    logout fix

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ document.getElementById("toBudget").addEventListener("click", function() {
 
 document.getElementById("logout").addEventListener("click", function() {
     cookie_name = "expense_tracker_cookie_container"
-    document.cookie = `${cookie_name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    window.location.href = 'https://landing.expense-tracker-demo.site/';
+    const now = new Date();
+    const expirationTime = new Date(now.getTime() - 15 * 60 * 1000);
+    //document.cookie = `${cookie_name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    document.cookie = `${cookie_name}=; domain=.expense-tracker-demo.site; expires=${expirationTime.toUTCString()}; path=/`;
+    window.location.href = 'https://expense-tracker-aytr.onrender.com';
 });
 
 document.addEventListener('DOMContentLoaded', function () {
